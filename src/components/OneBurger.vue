c2c901cbf87b9764f081edbc7406f0fece4c565a
Theodor-Bern <theodor.bernelind.0642@student.uu.se>
2024-12-01 19:47:47 +0100

    check

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
+  <div class="box">
+    <h3> {{ burger.name }} </h3>
+    <img v-bind:src="burger.imageURL" :alt="burger.name" style="width: 200px">
+    <ul>
+      <li><span>{{ burger.kcal }} kcal</span></li>
+      <li v-if="burger.gluten"><span class="allergy">Contains gluten</span></li>
+      <li v-if="burger.lactose"><span class="allergy">Contains lactose</span></li>
+      <li><span>Contains meat</span></li>
+    </ul>
+
+  
   </div>
 </template>
 
@@ -15,5 +24,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.box{
+  background-color: #444;
+    color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-size: 100%;
+}
+
+.allergy{
+    font-weight: bold;
+}
+
+ul li{
+    margin-bottom: 10px;
+}
 
 </style>
